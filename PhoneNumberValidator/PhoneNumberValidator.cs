cfb486b728064517aedf761db239c8f3687c5529
Nandita Vora <Nandita.Vora@in.niit.com>
2020-08-19 00:49:51 +0530

    initial commit

---
+++ PhoneNumberValidator/PhoneNumberValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PhoneNumberValidator
+{
+    public class PhoneNumberValidator
+    {
+        static void Main(string[] args)
+        {
+            // call the functions to validate phone number and display the status
+        }
+        public string GetInput()
+        {
+            // get phonenumber as input       
+        }
+        public void DisplayResult(int result)
+        {
+            // display status of phone number valid or invalid
+        }
+        public static int ValidatePhoneNumber(string input)
+        {
+            // validate phone-number
+            // for null or empty value, function should return -1
+            // for invalid phone-number, function should return 0
+            // for valid phone-number, function should return 1
+
+        }
+    }
+}
