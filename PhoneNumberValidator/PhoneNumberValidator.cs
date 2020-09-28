d31e17a22751c8c751b734e3a81d9b435f169570
shivansh <shivanshakb@gmail.com>
2020-09-28 14:21:46 +0530

    completed

--- PhoneNumberValidator/PhoneNumberValidator.cs
+++ PhoneNumberValidator/PhoneNumberValidator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
 
 namespace PhoneNumberValidator
 {
@@ -7,15 +9,32 @@ namespace PhoneNumberValidator
     {
         static void Main(string[] args)
         {
+            string userInput = GetInput();
             // call the functions to validate phone number and display the status
+            int result = ValidatePhoneNumber(userInput);
+            DisplayResult(result);
         }
         public static string GetInput()
         {
-            // get phonenumber as input       
+            // get phonenumber as input   
+            string userInput = Console.ReadLine();
+            return userInput;
         }
         public static void DisplayResult(int result)
         {
             // display status of phone number valid or invalid
+            if (result == -1)
+            {
+                Console.WriteLine("Empty value");
+            }
+            if (result == 1)
+            {
+                Console.WriteLine("Valid");
+            }
+            if (result == 0)
+            {
+                Console.WriteLine("Invalid");
+            }
         }
         public static int ValidatePhoneNumber(string input)
         {
@@ -24,6 +43,13 @@ namespace PhoneNumberValidator
             // for invalid phone-number, function should return 0
             // for valid phone-number, function should return 1
 
+            String str = input;
+            if (string.IsNullOrEmpty(str))
+                return -1;
+            if (Regex.IsMatch(str, @"^\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}[-]?\d{1}$"))
+                return 1;
+            else
+                return 0;
         }
     }
 }
